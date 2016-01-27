f5d01cd0ba8dfebc3d92c98d55341ac4563520a7
Jamie Robert Dawson <jamierobertdawson@gmail.com>
2016-01-26 22:09:16 -0500

    Passing Enums

---
+++ Passing enums/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.comboSports = New System.Windows.Forms.ComboBox()
+        Me.btnAnalyze = New System.Windows.Forms.Button()
+        Me.listEnumValues = New System.Windows.Forms.ListBox()
+        Me.listEnumNames = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'comboSports
+        '
+        Me.comboSports.FormattingEnabled = True
+        Me.comboSports.Items.AddRange(New Object() {"baseball", "basketball", "football"})
+        Me.comboSports.Location = New System.Drawing.Point(12, 21)
+        Me.comboSports.Name = "comboSports"
+        Me.comboSports.Size = New System.Drawing.Size(291, 21)
+        Me.comboSports.TabIndex = 0
+        '
+        'btnAnalyze
+        '
+        Me.btnAnalyze.Location = New System.Drawing.Point(84, 48)
+        Me.btnAnalyze.Name = "btnAnalyze"
+        Me.btnAnalyze.Size = New System.Drawing.Size(142, 26)
+        Me.btnAnalyze.TabIndex = 1
+        Me.btnAnalyze.Text = "Analyze Sport"
+        Me.btnAnalyze.UseVisualStyleBackColor = True
+        '
+        'listEnumValues
+        '
+        Me.listEnumValues.FormattingEnabled = True
+        Me.listEnumValues.Location = New System.Drawing.Point(12, 94)
+        Me.listEnumValues.Name = "listEnumValues"
+        Me.listEnumValues.Size = New System.Drawing.Size(120, 95)
+        Me.listEnumValues.TabIndex = 2
+        '
+        'listEnumNames
+        '
+        Me.listEnumNames.FormattingEnabled = True
+        Me.listEnumNames.Location = New System.Drawing.Point(151, 94)
+        Me.listEnumNames.Name = "listEnumNames"
+        Me.listEnumNames.Size = New System.Drawing.Size(120, 95)
+        Me.listEnumNames.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(328, 223)
+        Me.Controls.Add(Me.listEnumNames)
+        Me.Controls.Add(Me.listEnumValues)
+        Me.Controls.Add(Me.btnAnalyze)
+        Me.Controls.Add(Me.comboSports)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents comboSports As ComboBox
+    Friend WithEvents btnAnalyze As Button
+    Friend WithEvents listEnumValues As ListBox
+    Friend WithEvents listEnumNames As ListBox
+End Class
---
+++ Passing enums/Form1.vb
@@ -0,0 +1,54 @@
+﻿Public Class Form1
+    Private Enum Sports As Integer
+        baseball = 0
+        basketball = 1
+        football = 2
+
+    End Enum
+
+    Private mySport As Sports
+
+    Private Sub btnAnalyze_Click(sender As Object, e As EventArgs) Handles btnAnalyze.Click
+        clearList()
+        For Each item As Integer In System.Enum.GetValues(GetType(Sports))
+            listEnumValues.Items.Add(item.ToString())
+        Next
+
+
+
+
+        For Each item As String In System.Enum.GetNames(GetType(Sports))
+            listEnumNames.Items.Add(item)
+        Next
+
+        analyzeSport(mySport)
+    End Sub
+
+    Private Sub comboSports_SelectedIndexChanged(sender As Object, e As EventArgs) Handles comboSports.SelectedIndexChanged
+        If comboSports.SelectedIndex <> -1 Then
+            mySport = comboSports.SelectedIndex
+        End If
+
+
+    End Sub
+
+    Private Sub clearList()
+        listEnumNames.Items.Clear()
+        listEnumValues.Items.Clear()
+
+
+    End Sub
+
+    Private Sub analyzeSport(ByVal sportsList As Sports)
+        Select Case sportsList
+            Case Is = Sports.baseball
+                MessageBox.Show("Your sport is baseball")
+            Case Is = Sports.football
+                MessageBox.Show("Your sport is football")
+            Case Is = Sports.basketball
+                MsgBox("Your sport is basketball")
+            Case Else
+                MsgBox("You don't have a sport")
+        End Select
+    End Sub
+End Class
